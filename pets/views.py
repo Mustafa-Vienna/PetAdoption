e0fdac89842ab8e8d0641d22af80668ed48d3622
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-10-07 13:25:32 +0000

    Initial commit

---
+++ pets/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def index_page(request):
+    return render(request, "pets/index.html")
+
+
+def posts(request):
+    pass
+
+
+def post_detail(requests):
+    pass
