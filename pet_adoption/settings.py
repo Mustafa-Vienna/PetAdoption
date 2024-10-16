656d5328e07fe9a417217df5aa753aa74c108180
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-10-16 15:18:52 +0000

    Configure Debugging and Allowed host for deployment

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
