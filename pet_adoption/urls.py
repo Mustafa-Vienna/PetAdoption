b821f712003ed97ba36d5e10dde03793d5bef022
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-10-24 14:36:51 +0000

    Fix static media root in main urls

--- pet_adoption/urls.py
+++ pet_adoption/urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pets.urls')),
     path('accounts/', include('users.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
