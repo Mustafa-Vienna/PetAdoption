4980fdffd0b6fa60ef4f8dcf1ac80b6c42d6dbad
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-11-15 11:24:26 +0000

    Add 3-sec success message for comments in post-detail.html and script.js

--- pets/static/pets/js/script.js
+++ pets/static/pets/js/script.js
@@ -4,3 +4,11 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+
+$(document).ready(() => {
+  setTimeout(() => {
+    $("#successMsg").fadeOut().empty();
+  }, 3000);
+  $(".comment-form")[0]?.reset();
+});
