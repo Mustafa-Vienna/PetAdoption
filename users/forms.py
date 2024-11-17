f328602bec07d7b6bfa1a362ad29ea37a3fd60c6
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-11-17 20:35:15 +0000

    feat: add user registration form with custom validations

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator, MinLengthValidator
 
+name_validator = RegexValidator(
+    r'^[a-zA-Z0-9]*$', 'Username can only contain letters and numbers.'
+)
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    username = forms.CharField(
+        validators=[
+            MinLengthValidator(3, message="Username must be at least 3 characters long."),
+            name_validator
+        ],
+    )
 
     class Meta:
         model = User
