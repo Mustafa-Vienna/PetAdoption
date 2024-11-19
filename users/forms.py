f74e75d1df33be5ae7f74fd55d872b0afa96b49e
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-11-19 11:18:00 +0000

    Add validation for unique username and email with error message display

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator, MinLengthValidator
+from django.utils.html import format_html
 
 name_validator = RegexValidator(
     r'^[a-zA-Z0-9]*$', 'Username can only contain letters and numbers.'
@@ -19,3 +20,19 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+        
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(
+                format_html("This username already taken.<br>Please supply a different username!")
+                )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                format_html("This email is already in use.<br>Please supply a different email address!")
+                )
+        return email
