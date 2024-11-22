aa57111646a8676af4868c8ddcd9eb873d4b533e
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-11-22 13:54:35 +0000

    Add docstrings for forms.py and views.py in the users app

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,15 @@ from django.utils.html import format_html
 name_validator = RegexValidator(
     r'^[a-zA-Z0-9]*$', 'Username can only contain letters and numbers.'
 )
+'''
+A regex validator for username to ensure they contain only letters and numbers
+'''
 
 
 class UserRegisterForm(UserCreationForm):
+    '''
+    Form for registering new users with username and email validation
+    '''
     email = forms.EmailField()
     username = forms.CharField(
         validators=[
@@ -24,6 +30,11 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
     def clean_username(self):
+        '''
+        This function ensure the username is unique
+
+        Display error msg if the username is already exist.
+        '''
         username = self.cleaned_data.get("username")
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError(
@@ -35,6 +46,11 @@ class UserRegisterForm(UserCreationForm):
         return username
 
     def clean_email(self):
+        '''
+        This function ensure the email is unique
+
+        Display error msg if the email is already exist.
+        '''
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError(
