459ee9ed6406ba9500a9261350c6dca3cf4a7f22
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-11-22 14:50:34 +0000

    Add docsting to admin.py in pets app

--- pets/admin.py
+++ pets/admin.py
@@ -5,15 +5,26 @@ from .models import Post, Author, Tag, Comment
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
+    """
+    Admin settings for the post model:
+    filters, display fields, and slug generation
+    """
     list_filter = ("author", "tags", "date",)
     list_display = ("title", "date", "author",)
     prepopulated_fields = {"slug": ("title",)}
 
 
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin settings for the comment model:
+    Display author username and post in the admin list view
+    """
     list_display = ("author_username", "post")
 
     def author_username(self, user):
+        """
+        Get the username of the comment's author.
+        """
         return user.author.username
 
 
